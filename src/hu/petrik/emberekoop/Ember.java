6f6a0d468ed00d250cb8ca3323763b476cc381a9
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-22 16:45:55 +0200

    Emberek osztály kész

--- src/hu/petrik/emberekoop/Ember.java
+++ src/hu/petrik/emberekoop/Ember.java
@@ -1,5 +1,8 @@
 package hu.petrik.emberekoop;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Ember {
     private String nev;
     private String szuldatum;
@@ -22,10 +25,13 @@ public class Ember {
         String[] szuletesiAdatok = this.szuldatum.split("-");
         return  Integer.parseInt(szuletesiAdatok[2]);
     }
+    public int getEletkor() {
 
+        return LocalDate.now().getYear() - this.getSzuletesiEv();
+    }
     @Override
     public String toString() {
-        return String.format("%30s | %10s | %-20s", this.nev, this.szuldatum, this.szulhely);
+        return String.format("%30s | %10s (%3s)| %-20s ", this.nev, this.szuldatum,this.getEletkor(), this.szulhely);
     }
 
 }
